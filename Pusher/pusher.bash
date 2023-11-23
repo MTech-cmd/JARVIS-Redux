f5563905cdf9ad3a3b267df5be461c0390afccb0
MTech-cmd <mehdiek03@gmail.com>
2023-11-23 19:52:00 +0100

    fixed directory

--- Pusher/pusher.bash
+++ Pusher/pusher.bash
@@ -1,4 +1,4 @@
-cd "/home/mtech/Bit Academy/bit-academy/c-deep-dive-/01-newbie/02-hallo-wereld"
+cd "/home/mtech/Bit Academy/bit-academy/c-deep-dive/01-newbie/02-hallo-wereld"
 touch heat
 git add .
 git commit -m "heat"
