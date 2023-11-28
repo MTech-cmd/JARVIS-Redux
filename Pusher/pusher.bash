01405c7311e7c11feb4a784cc4b1bce828af90b4
MTech-cmd <mehdiek03@gmail.com>
2023-11-28 21:01:52 +0100

    fixed pusher directory to Nexed

--- Pusher/pusher.bash
+++ Pusher/pusher.bash
@@ -1,4 +1,4 @@
-cd "/home/mtech/Bit Academy/bit-academy/c-deep-dive/01-newbie/02-hallo-wereld"
+cd "/home/mtech/Bit Academy/bit-academy/javascript-beginner/05-arrays-loops/07-volgens-schema"
 touch heat
 git add .
 git commit -m "heat"
